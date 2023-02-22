fffb72bdf9a418ed6cf7214dfc0c1db329f37019
Matthew Luchini <luch@Matthews-MBP.lan>
2023-02-22 14:17:55 -0500

    Linq

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var GameNames = new List<string>() { "Super Mario", "COD", "Rocket League", "Destiny", };
+
+            GameNames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
+        
     }
 }
+
+
+//Create a list of video game names...
+//Order the list of games by length of the game name.
+//Use the lambda expression in this exercise as well.
+//use Method Syntax for this exercise
